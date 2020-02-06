b3bc6a3d40fe81afb400e7635995931f5c51b70b
Febriansyah <febriansyah@digiasia.asia>
2020-02-06 18:38:45 +0700

    add: hKit module

---
+++ Kit/src/main/java/com/hci/kit/constant/TagInjectConstant.kt
@@ -0,0 +1,9 @@
+package com.hci.kit.constant
+
+class TagInjectConstant {
+    companion object {
+        const val CONNECTIVITY_INTERCEPTOR = "TagInjectConstant.CONNECTIVITY_INTERCEPTOR"
+        const val RETROFIT = "TagInjectConstant.RETROFIT"
+        const val OKHTTP = "TagInjectConstant.OKHTTP"
+    }
+}
---
+++ Kit/src/main/java/com/hci/kit/extension/RxObserverExtension.kt
@@ -0,0 +1,24 @@
+package com.hci.kit.extension
+
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
+import io.reactivex.observers.DisposableCompletableObserver
+import io.reactivex.observers.DisposableMaybeObserver
+import io.reactivex.observers.DisposableSingleObserver
+
+
+fun <T> DisposableMaybeObserver<T>.disposedBy(compositeDisposable: CompositeDisposable) {
+    compositeDisposable.add(this)
+}
+
+fun <T> DisposableSingleObserver<T>.disposedBy(compositeDisposable: CompositeDisposable) {
+    compositeDisposable.add(this)
+}
+
+fun DisposableCompletableObserver.disposedBy(compositeDisposable: CompositeDisposable) {
+    compositeDisposable.add(this)
+}
+
+fun Disposable.disposedBy(compositeDisposable: CompositeDisposable) {
+    compositeDisposable.add(this)
+}
